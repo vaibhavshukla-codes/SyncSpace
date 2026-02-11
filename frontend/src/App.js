993c01459f977467db7e1c4ccdc0abfb3b496200
Vaibhav Shukla <vaibhavshukla565@gmail.com>
2026-02-11 18:43:02 +0530

    cleared app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // React Router = URL changes → Component changes → Page stays
+import LandingPage from './pages/landing';
 
 function App() {
   return (
-   
+    <>
+      <Router>
+
+        <Routes>
+          
+          {/* <Route path="/home" element={ } /> */}
+          
+          <Route path="/" element={<LandingPage/>} />
+          
+        </Routes>
+
+      </Router>
+    </>
   );
 }
 
